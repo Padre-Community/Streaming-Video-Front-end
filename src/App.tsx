2c813df9fd04fea0e62937afddae263c92ed2156
CEO-LIMA <carrlos345@gmail.com>
2026-07-24 18:02:30 -0300

    feat(UI): implementa componente Banner com layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import "./App.css";
 import Button from "./module/auth/components/Button";
 import Input from "./module/auth/components/Input";
+import { Banner } from "./module/UI/components/Banner";
 function App() {
   return (
     <div className="flex justify-center flex-col m-24 px-24">
       <h1 className="text-5xl font-bold flex items-center mb-8 justify-center">
         Streaming Video Front-end esta rodando, pho 🚀
       </h1>
-      <Input 
-      id= "email"
-      label= "email"
-      placeholder="digite seu email" />
+      <Input id="email" label="email" placeholder="digite seu email" />
 
       <Button text={"Login"} />
       <Button text={"Criar conta nova"} />
+      <Banner />
     </div>
   );
 }
--- src/module/UI/components/Banner.tsx
+++ src/module/UI/components/Banner.tsx
@@ -1,15 +1,21 @@
-import { bannerMock } from "./bannerMock";
+import { bannerMock } from "./mocks/bannerMock";
 
 export const Banner = () => {
   const featuredBanner =
     bannerMock.find((item) => item.isInDestaque) || bannerMock[0];
   return (
-    <section>
-      <img src={featuredBanner.Thumbnail_url} alt={featuredBanner.Title} />
-      {featuredBanner.isInDestaque && <span>DESTAQUE</span>}
-      <h1>{featuredBanner.Title}</h1>
-      <span>{featuredBanner.Category}</span>
-      <p>{featuredBanner.Description}</p>
+    <section className="relative w-full h-[450px] rounded-xl overflow-hidden">
+      <img
+        src={featuredBanner.thumbnail_Url}
+        alt={featuredBanner.title}
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute bottom-0 left-0 right-0 p-8 bg-gradient-to-t via-black/70 to-transparent text-white">
+        {featuredBanner.isInDestaque && <span>DESTAQUE</span>}
+        <h1>{featuredBanner.title}</h1>
+        <span>{featuredBanner.category}</span>
+        <p className="line-clamp-3 max-w-xl">{featuredBanner.description}</p>
+      </div>
     </section>
   );
 };
