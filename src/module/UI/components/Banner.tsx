238536240ae095729bce096845ba066e6010cc4e
CEO-LIMA <carrlos345@gmail.com>
2026-07-29 15:53:13 -0300

    feat(UI): adiciona componente UIButton e estiliza layout do Banner.

--- src/module/UI/components/Banner.tsx
+++ src/module/UI/components/Banner.tsx
@@ -1,4 +1,5 @@
 import { bannerMock } from "./mocks/bannerMock";
+import { UIbutton } from "./Button";
 
 export const Banner = () => {
   const featuredBanner =
@@ -19,10 +20,27 @@ export const Banner = () => {
         <h1 className="text-4xl font-bold text-red-600">
           {featuredBanner.title}
         </h1>
-        <span className="inline-block px-2.5 py-1 text-xs font-bold  tracking-wider text-white-400 bg-red-950/60 rounded-md mb-2">
+        <span className="inline-block px-2.5 text-xs font-bold  tracking-wider text-white-400 bg-red-950/60 rounded-md mb-2">
           {featuredBanner.category}
         </span>
-        <p className="max-w-xl text-gray-300 line-clamp-3 max-w-xl">
+        <div className="flex items-center gap-4 mt-4 my-3">
+          <UIbutton variant="primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span>Assistir</span>
+          </UIbutton>
+        </div>
+        <p className="max-w-xl text-gray-300 line-clamp-3 max-w-xl leading-relaxed">
           {featuredBanner.description}
         </p>
       </div>
