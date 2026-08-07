3c597823e9769cbe6fd7bf9cba91755801dfb93b
Thiago <thiagoloz2010@gmail.com>
2026-08-07 05:07:30 -0300

    ultimo ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import AppRoutes from "./routes/index.tsx";
-import { UIbutton } from "./UI/components/Button/Button.tsx";
-import useUploadVideoCard from "./module/auth/viewmodels/useUploadVideoCard.tsx";
-import UploadVideoCard from "./UI/components/UploadVideoCard/UploadVideoCard.tsx";
 
 function App() {
-  const { isOpen, handleOpen } = useUploadVideoCard();
-
   return (
     <>
-      <UIbutton onClick={handleOpen}>Publicar Vídeo</UIbutton>
-      <div className="grid place-items-center">
-        {isOpen && <UploadVideoCard handleOpen={handleOpen}></UploadVideoCard>}
-        {/*<AppRoutes />; */}
-
-        {/* Isso daqui deve ser retirado antes de enviar para o pull request */}
-      </div>
+      <AppRoutes />;
     </>
   );
 }
