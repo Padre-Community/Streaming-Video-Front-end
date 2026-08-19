2f55ede364f127430739ba2f05447206587708d8
CEO-LIMA <carrlos345@gmail.com>
2026-08-19 17:24:52 -0300

    feat: adiciona estado controlado e submisao ao SearchBar

--- src/UI/components/Navbar/SearchBar.tsx
+++ src/UI/components/Navbar/SearchBar.tsx
@@ -1,20 +1,25 @@
 import { Search } from "lucide-react";
 import { Input } from "../Input/Input";
-import type { FormEvent } from "react";
+import { useState, type FormEvent } from "react";
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
 }
 
 export const SearchBar = ({ onSearch }: SearchBarProps) => {
+  const [query, setQuery] = useState("");
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSearch("");
+    onSearch(query);
   };
   return (
     <form onSubmit={handleSubmit} className="relative w-full max-w-md">
       <Search size={16} className="absolute left-3 top-1/2 -translate-y-1/2" />;
-      <Input className="pl-10 rounded-full border-border text-text-secondary" />
+      <Input
+        className="pl-10 rounded-full border-border text-text-secondary"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
     </form>
   );
 };
