49b0eb5fe8e207d8c3043ba418f6fd357ad7faf8
CEO-LIMA <carrlos345@gmail.com>
2026-08-20 18:54:32 -0300

    feat: adiciona o bloco do usuario e finaliza o botão de Publicar.

--- src/UI/components/Navbar/Navbar.tsx
+++ src/UI/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { User } from "lucide-react";
 import { Upload } from "lucide-react";
 import { Sun } from "lucide-react";
 import { Moon } from "lucide-react";
@@ -13,7 +14,7 @@ export const Navbar = () => {
 
   return (
     <header className="w-full border-b border-border px-6 py-3 mb-6">
-      <nav className="flex items-center justify-between">
+      <nav className="flex items-center justify-between pl-3 pr-3">
         <div>
           <img
             className="h-10 w-auto object-contain"
@@ -40,12 +41,24 @@ export const Navbar = () => {
             className="rounded-4xl"
             onClick={toggleTheme}
           >
-            <span className="text-text-on-brand text-sm">
-              <Upload size={16} />
-              Upload
+            <span className="inline-flex text-text-on-brand text-sm pl-2 pr-2">
+              <Upload size={16} className="mr-2 mt-0.5" />
+              Publicar
             </span>
           </UIbutton>
-          <div></div>
+          <div className=" flex items-center gap-3 cursor-pointer border-l border-border pl-3">
+            <div className="flex flex-col text-right">
+              <span className="text-sm font-semibold text-text-primary">
+                Loid Padre
+              </span>
+              <span className="text-xs font-semibold text-red-500">
+                MEU CANAL
+              </span>
+            </div>
+            <div className="flex items-center justify-center w-9 h-9 rounded-full border border-border bg-surface text-text-secondary hover:bg-brand-hover transition-colors">
+              <User size={18} />
+            </div>
+          </div>
         </div>
       </nav>
     </header>
