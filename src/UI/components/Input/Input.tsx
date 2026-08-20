0f2766d9decfc5160dcf5935a16369025302d645
CEO-LIMA <carrlos345@gmail.com>
2026-08-19 23:39:27 -0300

    refactor: altera alguns estilos com varaiveis do index.css

--- src/UI/components/Input/Input.tsx
+++ src/UI/components/Input/Input.tsx
@@ -18,10 +18,10 @@ export const Input = ({
           {labelIcon} {labelText}
         </label>
       )}
-      ;
+
       <input
         id={id}
-        className={`bg-zinc-800 p-2 w-full text-zinc-100 rounded-[0.4rem] ${className || ""}`}
+        className={`bg-surface p-2 w-ful rounded-[0.4rem] ${className || ""}`}
         {...rest}
       />
     </div>
