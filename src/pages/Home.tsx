353f737c9aba4a11116a089f8e48d6d519f3d020
CEO-LIMA <carrlos345@gmail.com>
2026-08-28 16:00:22 -0300

    feat(Home): adiciona logica de estado para chamar card upload via button navbar.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
 import { Navbar } from "../UI/components/Navbar/Navbar";
 import { Banner } from "../UI/components/Banner/Banner";
+import { useState } from "react";
 import VideoView from "../module/video/VideoView/VideoView";
+import UploadVideoCard from "../UI/components/UploadVideoCard/UploadVideoCard";
+
 export default function Home() {
+  const [isUploadOpen, setUploadOpen] = useState(false);
+  console.log("Estado atual:", isUploadOpen);
   return (
     <div>
-      <Navbar />
+      <Navbar onOpenUpload={() => setUploadOpen(true)} />
       <Banner />
       <VideoView />
+      {isUploadOpen ? (
+        <UploadVideoCard handleOpen={() => setUploadOpen(false)} />
+      ) : null}
     </div>
   );
 }
